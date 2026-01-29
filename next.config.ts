aaea695d25492b5986ee903487b78d2c3101df83
Nandini Saini <n9a8n1d0i9n1i@gmail.com>
2026-01-29 15:15:06 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://ekmepja

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ekmepjadtgoemytvbpnk.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
